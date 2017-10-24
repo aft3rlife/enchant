f9989965752b0c37f0a071b78abb760c21ab163a
Marc Boisvert <marc@localhost.localdomain>
2017-10-24 12:24:43 -0400

    update deps, initial work on global variables

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 const postcssConfig = {
   plugins: {
     'postcss-import': {},
+    'postcss-custom-properties': {},
+    'postcss-color-function': {},
+    'postcss-nested': {},
     'postcss-cssnext': {},
     'postcss-normalize': {
       allowDuplicates: true
